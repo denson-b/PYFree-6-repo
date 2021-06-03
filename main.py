7392d45a3308ba8ff55bf8cf9ddb94b671907f24
denson101 <>
2021-06-03 16:25:54 +0000

    Finished task

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 # Comment
 # print("hello world")
 date_str = input('Введите дату: ')
-task_str = input('Введите задание: ')
+task_str = input('Введите задачу: ')
 print(date_str, task_str)
+
+date_str = input('Введите дату: ')
+task_str = input('Введите задачу: ')
+date_str2 = input('Введите дату: ')
+task_str2 = input('Введите задачу: ')
+date_str3 = input('Введите дату: ')
+task_str3 = input('Введите задачу: ')
+print(date_str, task_str)
+print(date_str2, task_str2)
+print(date_str3, task_str3)
+
+task_dic = {date_str:task_str, 
+date_str2:task_str2, 
+date_str3:task_str3}
+
+print(task_dic)
